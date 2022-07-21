8ef131c3b0bd6ef8f4c887ef232d9b671ca47608
becouif <lovguya@gmail.com>
2022-07-21 11:55:05 +0300

    update

--- classes.ts
+++ classes.ts
@@ -2,6 +2,9 @@ class Vehicle {
   protected drive(): void {
     console.log('Moving!!!');
   }
+  // to accept argument in the class you need a constructor like below
+
+  constructor(public color: string) {}
   honk(): void {
     console.log('Beep');
   }
@@ -13,6 +16,9 @@ class Vehicle {
 // 'extends' works like copy and paste
 //  note: extend can be overwritten in the child class
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   move(): void {
     console.log('Chugga Chugga');
     this.drive();
@@ -28,8 +34,10 @@ class Motorcycle {
   }
 }
 
-const vehicle = new Vehicle();
-const car = new Car();
+const vehicle = new Vehicle('Lime');
+console.log(vehicle.color);
+
+const car = new Car(4, 'yellow');
 const motorcycle = new Motorcycle();
 car.move();
 car.honk();
@@ -37,6 +45,6 @@ motorcycle.startRiding();
 
 // differences between ES15 and types in typescript
 // MODIFIERS
-//  =PUBLIC method can be called anywhere, any toime
+//  =PUBLIC method can be called anywhere, any time
 //  = private: method can only be called by other methods in this class
 //  protected: method can be called by other methods in this class, or by other methods in child classes
